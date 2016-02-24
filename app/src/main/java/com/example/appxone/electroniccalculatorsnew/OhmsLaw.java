340341a4dafb586eb7f664e2aa92e46824da67a0
Muhammad sohail <syedmuhammadsohail@yahoo.com>
2016-02-24 13:24:30 +0500

    Version 1.4

---
+++ app/src/main/java/com/example/appxone/electroniccalculatorsnew/OhmsLaw.java
@@ -0,0 +1,15 @@
+package com.example.appxone.electroniccalculatorsnew;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by APPXONE on 2/24/2016.
+ */
+public class OhmsLaw extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ohms_law);
+    }
+}
